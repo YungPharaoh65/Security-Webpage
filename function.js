2dd3b82f8584ac968f911b9bf852a915da370f11
YungPharaoh65 <princemsimango27@gmail.com>
2024-08-30 15:57:08 +0200

    made changes on pages and added javascript

---
+++ function.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the button with the class 'startbtn'
+    const startBtn = document.querySelector('.startbtn');
+
+    // Add a click event listener to the button
+    startBtn.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default behavior
+
+        // Select the 'About Us' section
+        const aboutUsSection = document.getElementById('about-us');
+        
+
+        // Scroll to the 'About Us' section smoothly
+        aboutUsSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
